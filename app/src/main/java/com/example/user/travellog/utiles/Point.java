a6310d04618cb281007601e95db6e9307f27d675
44x1carbon <b4079@oic.jp>
2016-12-02 04:56:56 +0900

    コンストラクタ&distanceメソッド追加

--- app/src/main/java/com/example/user/travellog/utiles/Point.java
+++ app/src/main/java/com/example/user/travellog/utiles/Point.java
@@ -4,6 +4,8 @@ import java.sql.Time;
 import java.util.Calendar;
 import java.util.Date;
 
+import jp.co.yahoo.android.maps.GeoPoint;
+
 /**
  * Created by user on 2016/12/02.
  */
@@ -29,8 +31,48 @@ public class Point {
         this.transitTime = new Time(time);
     }
 
+    Point(GeoPoint location, Date date){
+        this.location = new Location(location);
+
+        Calendar _date = Calendar.getInstance();
+        _date.setTime(date);
+        this.transitTime = new Time(_date.getTimeInMillis());
+    }
+
+    Point(GeoPoint location, Calendar date){
+        this.location = new Location(location);
+        this.transitTime = new Time(date.getTimeInMillis());
+    }
+
+    Point(GeoPoint location, long time){
+        this.location = new Location(location);
+        this.transitTime = new Time(time);
+    }
+
+    Point(double lat, double lng, Date date){
+        this.location = new Location(lat, lng);
+
+        Calendar _date = Calendar.getInstance();
+        _date.setTime(date);
+        this.transitTime = new Time(_date.getTimeInMillis());
+    }
+
+    Point(double lat, double lng, Calendar date){
+        this.location = new Location(lat, lng);
+        this.transitTime = new Time(date.getTimeInMillis());
+    }
+
+    Point(double lat, double lng, long time){
+        this.location = new Location(lat, lng);
+        this.transitTime = new Time(time);
+    }
+
     public long timeDiff(Point p){
         return transitTime.getTime() - p.getTransitTime().getTime();
+    }
+
+    public double distance(Point p){
+        return this.location.distance(p.getLocation());
     }
 
     public Location getLocation() {
